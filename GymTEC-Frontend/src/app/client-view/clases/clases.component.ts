0d1bacebcec15f28f3434490c9f37c8815876efa
IgnacioCarazo <nachocarazo18@gmail.com>
2021-06-18 19:44:07 -0600

    Esqueleto de Busqueda y Registo de Clases

--- GymTEC-Frontend/src/app/client-view/clases/clases.component.ts
+++ GymTEC-Frontend/src/app/client-view/clases/clases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clases',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clases.component.css']
 })
 export class ClasesComponent implements OnInit {
+  form!: FormGroup;
+  form2!: FormGroup;
+  
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
+    //this.initForm2();
+
+  }
+
+
+  onSubmit() {      
+    console.log(this.form.value);
+    console.log(this.form2.value);
+
+    this.form.reset();
+  }
+
+  /**
+  * @name initForm()
+  * @description If its edit mode it will load the inputs of the form with the preexistent values of the recipe. Otherwise
+  * it will just set thes values 'empty' for the user to fill.
+  */
+   private initForm() {
+    let gymClass = '';
+    let gym = '';
+
+    this.form = new FormGroup({
+      gymClass: new FormControl(gymClass),
+      gym: new FormControl(gym),
+    });
+  }
+
+  /**
+  * @name initForm2()
+  * @description If its edit mode it will load the inputs of the form with the preexistent values of the recipe. Otherwise
+  * it will just set thes values 'empty' for the user to fill.
+  */
+   private initForm2() {
+    let gymClassID = 0;
+
+    this.form = new FormGroup({
+      gymClassID: new FormControl(gymClassID),
+    });
   }
 
 }
