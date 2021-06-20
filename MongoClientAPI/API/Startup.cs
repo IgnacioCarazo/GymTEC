9b16c25f40b95445cc1bdaa8b0c86c7f4c4de175
Joseph <josephdjz@hotmail.com>
2021-06-20 15:19:54 -0600

    Api para la base de datos Mongo
    Api para acceder a la base de datos en Azure

---
+++ MongoClientAPI/API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoClientAPI.API;
+using MongoClientAPI.API.services;
+using MongoDB.Driver;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Security.Authentication;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace MongoClientAPI.API{
+
+	class Startup : FunctionsStartup{
+        public override void Configure(IFunctionsHostBuilder builder){
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(config["ConnectionString"]));
+   
+            settings.ConnectTimeout = TimeSpan.FromSeconds(60);
+            settings.SocketTimeout = TimeSpan.FromMinutes(5);
+            settings.MaxConnectionIdleTime = TimeSpan.FromMinutes(3);
+            void SocketConfigurator(Socket s) => s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+            settings.ClusterConfigurator = builder => builder.ConfigureTcp(tcp => tcp.With(socketConfigurator: (Action<Socket>)SocketConfigurator));
+
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+
+            builder.Services.AddSingleton((s) => new MongoClient(settings));
+            builder.Services.AddTransient<IClientService, ClientService>();
+        }
+    }
+}
---
+++ MongoClientAPI/API/functions/GetAllClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using MongoClientAPI.API.services;
+
+namespace MongoClientAPI.API.functions{
+    /// <summary>
+    /// Crea la direccion en la cual se obtiene todos los clientes.
+    /// </summary>
+    /// <returns>
+    /// StatusCodeResult 404 y 500
+    /// </returns>
+    class GetAllClient
+    {
+        private readonly ILogger<GetAllClient> _logger;
+        private readonly IClientService _clientService;
+        public GetAllClient(ILogger<GetAllClient> logger, IClientService clientService){
+            _logger = logger;
+            _clientService = clientService;
+        }
+
+        [FunctionName(nameof(GetAllClient))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Clients")] HttpRequest req){
+            IActionResult result;
+            try{
+                var clients = await _clientService.GetClients();
+                if (clients == null){
+                    _logger.LogWarning("No Clients found!");
+                    result = new StatusCodeResult(StatusCodes.Status404NotFound);
+                }
+                result = new OkObjectResult(clients);
+            }catch (Exception ex){
+                _logger.LogError($"Internal Server Error. Exception thrown: {ex.Message}");
+                result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return result;
+        }
+    }
+}
